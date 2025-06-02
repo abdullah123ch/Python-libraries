e732d869d55aeb8563937c8400356546c796ce86
abdullah123ch <abdullah.chaudhary1605@gmail.com>
2025-06-02 22:12:19 +0500

    add file

--- PyWhatkit.py
+++ PyWhatkit.py
@@ -1,7 +1,7 @@
-import pywhatkit
+#import pywhatkit
 # PyWhatkit is a versatile Python library designed for automating WhatsApp messaging and other tasks.
 
-1. #WhatsApp Automation:**
+1. #WhatsApp Automation:
    # - Effortlessly sending WhatsApp messages to individual contacts or groups, streamlining communication processes.
    # - Implementing sophisticated message scheduling, allowing users to set specific times for message delivery.
     
@@ -11,8 +11,7 @@ import pywhatkit
     
 #pywhatkit.sendwhatmsg_instantly(phone_no: str, message: str, wait_time: int = 15, tab_close: bool = False, close_time: int = 3)
     
-   # - Facilitating the seamless transmission of multimedia content, including images and videos, enhancing the richness of communication.
-    
+   # - Facilitating the seamless transmission of multimedia content, including images and videos, enhancing the richness of communication.   
 
 #pywhatkit.sendwhats_image(receiver: str, img_path: str, caption: str = "", wait_time: int = 15, tab_close: bool = False, close_time: int = 3) -> None
     
